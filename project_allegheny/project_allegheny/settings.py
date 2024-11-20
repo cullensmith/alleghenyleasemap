9a42f2258cc65a6a77cf9fb9598358558cb26d51
Cullen Smith <smith.cmaps@gmail.com>
2024-11-20 11:51:51 -0500

    updated to filesystem cache

--- project_allegheny/project_allegheny/settings.py
+++ project_allegheny/project_allegheny/settings.py
@@ -128,10 +128,30 @@ STATICFILES_DIRS = [
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',  # For in-memory caching
-        'LOCATION': 'unique-snowflake',  # This is an identifier for the cache
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/dev/shm/django_cache',  # Cache location in shared memory
+        'OPTIONS': {
+            # 'MAX_ENTRIES': 1000,  # Maximum number of cache entries
+        },
     }
 }
+
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',  # For in-memory caching
+#         'LOCATION': 'unique-snowflake',  # This is an identifier for the cache
+#     }
+# }
+
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django_redis.cache.RedisCache',
+#         'LOCATION': 'redis://127.0.0.1:6379/1',  # Change to your Redis server address
+#         'OPTIONS': {
+#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+#         }
+#     }
+# }
 
 MEDIA_URL = 'media/'
 
