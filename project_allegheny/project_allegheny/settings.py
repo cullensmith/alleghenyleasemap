29b0b3104b4dce207e34c412581735bc79921c37
Cullen Smith <smith.cmaps@gmail.com>
2024-12-02 09:52:05 -0500

    moved apply button to top

--- project_allegheny/project_allegheny/settings.py
+++ project_allegheny/project_allegheny/settings.py
@@ -126,22 +126,23 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': '/dev/shm/django_cache',  # Cache location in shared memory
-        'OPTIONS': {
-            # 'MAX_ENTRIES': 1000,  # Maximum number of cache entries
-        },
-    }
-}
 
 # CACHES = {
 #     'default': {
-#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',  # For in-memory caching
-#         'LOCATION': 'unique-snowflake',  # This is an identifier for the cache
+#         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+#         'LOCATION': '/dev/shm/django_cache',  # Cache location in shared memory
+#         'OPTIONS': {
+#             # 'MAX_ENTRIES': 1000,  # Maximum number of cache entries
+#         },
 #     }
 # }
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',  # For in-memory caching
+        'LOCATION': 'unique-snowflake',  # This is an identifier for the cache
+    }
+}
 
 # CACHES = {
 #     'default': {
